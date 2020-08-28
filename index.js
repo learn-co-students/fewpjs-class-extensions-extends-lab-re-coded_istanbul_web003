d1c5efb7afbd5d1df269b23e6e76f6129982f6e4
Majd Ajroudi <majd.ajroudi@gmail.com>
2020-08-28 14:16:37 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
-// Your code here
+// // Your code here
+// // Your code here
+
+class Polygon {
+    constructor(arrOfInts){
+        this.arr = arrOfInts
+    }
+
+    get countSides(){
+        return this.arr.length 
+    }
+
+    get perimeter(){
+        let sum = 0;
+        for (const side of this.arr){
+            sum += side
+        }
+        return sum
+    }
+}
+
+class Triangle extends Polygon{
+    constructor(arrOfInts){
+        super(arrOfInts)
+    }
+
+    get isValid(){
+        if (this.arr[0]+this.arr[1] > this.arr[2]){
+            if (this.arr[1]+this.arr[2] > this.arr[0]){
+                if(this.arr[0]+this.arr[2] > this.arr[1]){
+                    return true
+                }
+            }
+        }
+        return false
+    }
+}
+
+class Square extends Polygon {
+    constructor(arrOfInts){
+        super(arrOfInts)
+    }
+    get isValid(){
+        if (this.arr.every( (side, i, array) => side === array[0])){
+            return true
+        }
+        else{
+            return false
+        }
+        
+    }
+
+    get area(){
+        return this.arr[0]**2;
+    }
+}
